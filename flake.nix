d7adf721b76d9309f1fcedeec0ccebb69031f6f3
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2022-08-24 21:28:18 -0400

    v1.48.0

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "A flake defining golangci-linux binary package via GitHub releases";
+
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    golangci-lint-x86_64-linux = {
+      url = "https://github.com/golangci/golangci-lint/releases/download/v1.48.0/golangci-lint-1.48.0-linux-amd64.tar.gz";
+      flake = false;
+    };
+    golangci-lint-x86_64-darwin = {
+      url = "https://github.com/golangci/golangci-lint/releases/download/v1.48.0/golangci-lint-1.48.0-darwin-amd64.tar.gz";
+      flake = false;
+    };
+  };
+
+  outputs =
+    { self,
+      nixpkgs,
+      golangci-lint-x86_64-linux,
+      golangci-lint-x86_64-darwin,
+    }:
+
+    let
+      package = { system, src }:
+        let
+          pkgs = import nixpkgs { system = system; };
+        in pkgs.stdenv.mkDerivation {
+          name = "golangci-lint-1.48.0";
+          version = "1.48.0";
+          src = src;
+          installPhase = "mkdir -p $out/bin && cp $src/golangci-lint $out/bin/";
+        };
+    in {
+      packages.x86_64-linux.default = package {
+        system = "x86_64-linux";
+        src = golangci-lint-x86_64-linux;
+      };
+      packages.x86_64-darwin.default = package {
+        system = "x86_64-darwin";
+        src = golangci-lint-x86_64-darwin;
+      };
+    };
+}
