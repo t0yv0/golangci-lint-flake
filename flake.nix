3bfba0697c655a0d0ea215ee070f954dbd245fe0
Anton Tayanovskyy <anton@pulumi.com>
2022-12-06 11:18:03 -0500

    Update to 1.50.1

--- flake.nix
+++ flake.nix
@@ -4,11 +4,11 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
     golangci-lint-x86_64-linux = {
-      url = "https://github.com/golangci/golangci-lint/releases/download/v1.49.0/golangci-lint-1.49.0-linux-amd64.tar.gz";
+      url = "https://github.com/golangci/golangci-lint/releases/download/v1.50.1/golangci-lint-1.50.1-linux-amd64.tar.gz";
       flake = false;
     };
     golangci-lint-x86_64-darwin = {
-      url = "https://github.com/golangci/golangci-lint/releases/download/v1.49.0/golangci-lint-1.49.0-darwin-amd64.tar.gz";
+      url = "https://github.com/golangci/golangci-lint/releases/download/v1.50.1/golangci-lint-1.50.1-darwin-amd64.tar.gz";
       flake = false;
     };
   };
@@ -25,8 +25,8 @@
         let
           pkgs = import nixpkgs { system = system; };
         in pkgs.stdenv.mkDerivation {
-          name = "golangci-lint-1.49.0";
-          version = "1.49.0";
+          name = "golangci-lint-1.50.1";
+          version = "1.50.1";
           src = src;
           installPhase = "mkdir -p $out/bin && cp $src/golangci-lint $out/bin/";
         };
